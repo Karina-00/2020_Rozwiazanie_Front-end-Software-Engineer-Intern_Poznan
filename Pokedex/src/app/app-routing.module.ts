51fc29b6ec494208a625291b816684c7e860f160
Karina-00 <karina1szubert@gmail.com>
2020-04-09 17:42:59 +0200

    topbar init version

--- Pokedex/src/app/app-routing.module.ts
+++ Pokedex/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
+import { PokedexComponent } from './pokedex/pokedex.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomepageComponent,
+  },
+  {
+    path: 'pokedex',
+    component: PokedexComponent,
   },
 ];
 
